45f039dd77429341d7daafc58647a7b6c72a105f
Emirhan Güler <emirhangueler52@gmail.com>
2022-10-27 21:09:05 +0200

    Init processor service

---
+++ processor/src/test/java/com/emirhangueler/processor/ProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emirhangueler.processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
