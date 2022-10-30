d9bb3d689c6986f21888dade61a878e5cd90fd56
Emirhan Güler <emirhangueler52@gmail.com>
2022-10-31 00:40:36 +0100

    Remove test functionality of LogProcessor

--- processor/src/main/java/com/emirhangueler/processor/processors/LogProcessor.java
+++ processor/src/main/java/com/emirhangueler/processor/processors/LogProcessor.java
@@ -8,15 +8,6 @@ import org.springframework.stereotype.Component;
 @Component
 public final class LogProcessor implements Processor {
     public void process(final String message) {
-        double r = Math.random() * 10000;
-        System.out.println("Message %s".formatted(message));
-        System.out.println("Wait for %.2f ms".formatted(r));
-        try {
-            Thread.sleep(((long) r));
-        } catch (InterruptedException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        System.out.println("Wait finished");
+        System.out.println("Message <%s>".formatted(message));
     }
 }
